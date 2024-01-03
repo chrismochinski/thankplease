c693162eef6efbb6c1664ce5849d10eaad65aff4
Chris Mochinski <cmochinski@gmail.com>
2024-01-03 00:04:18 -0600

    FINALLY...no errors...stacked...wut it do

--- build/index.js
+++ build/index.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 5000;
+const port = 3001;
 app.get('/', (req, res) => res.send('Welcome to thankplease!'));
 app.listen(port, () => console.log(`Server running on http://localhost:${port} wut it dooooooooo`));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = 5000;
+const port = 3001;
 
 app.get('/', (req, res) => res.send('Welcome to thankplease!'));
 
