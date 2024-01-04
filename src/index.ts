49e77a8e5a8d96529890f3444733175e0eb13d53
Chris Mochinski <cmochinski@gmail.com>
2024-01-04 03:04:26 -0600

    post route good!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Pool } from "pg";
 import express from "express";
+import cors from "cors";
 
 interface TransactionRequest {
   ticker: string;
@@ -11,6 +12,7 @@ interface TransactionRequest {
 }
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
 const port = 3001;
 
 app.get("/", (req, res) => res.send("Welcome to thankplease!"));
@@ -25,12 +27,26 @@ const pool = new Pool({
 });
 
 app.post("/api/transactions", async (req: express.Request, res: express.Response) => {
-  const { ticker, quantity, isNewTotal }: TransactionRequest = req.body;
+  const {
+    ticker,
+    quantity,
+    isNewTotal,
+    dateOfTransaction,
+    timeOfTransaction,
+    timeZoneOfTransaction,
+  }: TransactionRequest = req.body;
 
   try {
     const totalHolding = parseFloat(quantity);
     // Use isNewTotal to determine whether to add or replace the total holding
-    await addOrUpdateAsset(ticker, totalHolding, isNewTotal);
+    await addOrUpdateAsset(
+      ticker,
+      totalHolding,
+      isNewTotal,
+      dateOfTransaction,
+      timeOfTransaction,
+      timeZoneOfTransaction
+    );
     res.json({ message: "Transaction recorded successfully." });
   } catch (error) {
     console.error("Heckin' Error!:", error);
@@ -38,27 +54,43 @@ app.post("/api/transactions", async (req: express.Request, res: express.Response
   }
 });
 
-async function addOrUpdateAsset(ticker: string, totalHolding: number, isNewTotal: boolean) {
+
+async function addOrUpdateAsset(
+  ticker: string,
+  totalHolding: number,
+  isNewTotal: boolean,
+  dateOfTransaction: string,
+  timeOfTransaction: string,
+  timeZoneOfTransaction: string
+) {
   const client = await pool.connect();
 
   try {
     const checkRes = await client.query("SELECT * FROM user_assets WHERE ticker = $1", [ticker]);
     if (checkRes.rows.length > 0) {
+      // If isNewTotal is true, replace the current total with the new value
+      // Otherwise, add the new amount to the existing total
       const currentTotalHolding = parseFloat(checkRes.rows[0].total_holding);
-      const newTotalHolding = isNewTotal ? totalHolding : currentTotalHolding + totalHolding;
-      await client.query("UPDATE user_assets SET total_holding = $1 WHERE ticker = $2", [
-        newTotalHolding,
-        ticker,
-      ]);
+      const newTotalHolding = isNewTotal ? currentTotalHolding + totalHolding : totalHolding;
+
+      await client.query(
+        "UPDATE user_assets SET total_holding = $1, date_of_transaction = $2, time_of_transaction = $3, timezone = $4 WHERE ticker = $5",
+        [newTotalHolding, dateOfTransaction, timeOfTransaction, timeZoneOfTransaction, ticker]
+      );
     } else {
-      await client.query("INSERT INTO user_assets (ticker, total_holding) VALUES ($1, $2)", [
-        ticker,
-        totalHolding,
-      ]);
+      await client.query(
+        "INSERT INTO user_assets (ticker, total_holding, date_of_transaction, time_of_transaction, timezone) VALUES ($1, $2, $3, $4, $5)",
+        [ticker, totalHolding, dateOfTransaction, timeOfTransaction, timeZoneOfTransaction]
+      );
     }
+    console.log("Successfully added or updated asset:", ticker);
+    console.log("Total holding:", totalHolding);
+    console.log("Date of transaction:", dateOfTransaction);
+    console.log("Time of transaction:", timeOfTransaction);
+    console.log("Timezone of transaction:", timeZoneOfTransaction);
   } catch (error) {
     console.error("Error in addOrUpdateAsset:", error);
-    throw error; // rethrow the error for proper error handling in the route
+    throw error;
   } finally {
     client.release();
   }
