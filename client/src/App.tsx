3f43a7cd112e5cf3d693028803148cdbbd6fe339
Chris Mochinski <cmochinski@gmail.com>
2024-01-19 10:26:55 -0600

    market page and react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import { FetchCoinGeckoList } from "./FetchCoinGeckoData/FetchCoinGeckoData";
 import { Home } from "./Home/Home";
@@ -5,13 +6,20 @@ import { SvgMiddleBlob } from "./SvgMiddleBlob/SvgMiddleBlob";
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <SvgMiddleBlob />
+      <Routes>
+
       <Home />
       <div className="button-row p-large align-start z-index-top">
+        
         <FetchCoinGeckoList />
       </div>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ client/src/Market/Market.tsx
@@ -0,0 +1,9 @@
+import './Market.scss'
+
+export function  Market(): JSX.Element {
+    return (
+        <div>
+            <h1>Market</h1>
+        </div>
+    )
+}
